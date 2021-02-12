2aaabb7a18bdb6ba65fb52f8d046de066e5cb18a
joriszandbergen <62294353+joriszandbergen@users.noreply.github.com>
2021-02-12 10:14:09 +0100

    setup of boilerplate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import Global style document
+import GlobalStyles from "./components/GlobalStyles";
+//import Router
+import { Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <h1>EQUI web-app fjdsanl dusaiiou dhsaibfksak </h1>
+    </div>
+  );
+}
+
+export default App;
