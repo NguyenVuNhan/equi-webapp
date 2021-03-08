0eef6dd4c70f1037d7eb51c43538f4ac7626702e
joriszandbergen <62294353+joriszandbergen@users.noreply.github.com>
2021-03-08 09:40:15 +0100

    added an api code

--- src/WelcomeScreen/batteryDisplay.js
+++ src/WelcomeScreen/batteryDisplay.js
@@ -16,18 +16,13 @@ const batteryDisplay = (p) => {
   };
 
   p.keyPressed = function () {
-    if (p.keyCode === p.RIGHT_ARROW) 
-    {
+    if (p.keyCode === p.RIGHT_ARROW) {
       batteryLevel += 5;
-    }
-    else if (p.keyCode === p.LEFT_ARROW) 
-    {
+    } else if (p.keyCode === p.LEFT_ARROW) {
       batteryLevel -= 5;
-    }
-    else if (p.keyCode === p.UP_ARROW)
-    {
-      console.log('x:' + p.mouseX);
-      console.log('y:' + p.mouseY);
+    } else if (p.keyCode === p.UP_ARROW) {
+      console.log("x:" + p.mouseX);
+      console.log("y:" + p.mouseY);
     }
   };
 
@@ -50,7 +45,7 @@ const batteryDisplay = (p) => {
     p.line(0, 0, 300, 300); // x1 , y1, x2, y2
     p.pop();*/
 
-    p.push(); 
+    p.push();
     p.translate(cx, cy); // move to center of screen
     p.rotate(180);
     p.noStroke();
@@ -60,50 +55,37 @@ const batteryDisplay = (p) => {
     p.pop();
   };
 
-  function MakeWaveLine(x1, x2, y)
-  {
+  function MakeWaveLine(x1, x2, y) {
     p.noStroke();
-    for(let i=x1;i<=x2; i+=15)
-    {
-      let diameter = p.random(20,25);
+    for (let i = x1; i <= x2; i += 15) {
+      let diameter = p.random(20, 25);
       MoveWaveLine(x2, y);
-      p.ellipse(i, yvalues[i], diameter,diameter);
+      p.ellipse(i, yvalues[i], diameter, diameter);
     }
   }
 
-  function MoveWaveLine(cx2, y)
-  {
-    p.theta+=0.05;
-    for (let i = 0; i < cx2+10; i++) {
-      if(i%2==0)
-      yvalues[i] = y+p.random(2,4);
-      else
-      yvalues[i] = y-p.random(1,2);
+  function MoveWaveLine(cx2, y) {
+    p.theta += 0.05;
+    for (let i = 0; i < cx2 + 10; i++) {
+      if (i % 2 == 0) yvalues[i] = y + p.random(2, 4);
+      else yvalues[i] = y - p.random(1, 2);
     }
 
     let x = p.theta;
-    for (let i = 0; i < 100; i++) 
-    {
-        // Every other wave is cosine instead of sine
-        if (i % 2 == 0) 
-        {
-          yvalues[i] += p.sin(x) * 2;
-        }
-        else 
-        {
-          yvalues[i] += p.cos(x) * 2;
-        }
-          x = y;
+    for (let i = 0; i < 100; i++) {
+      // Every other wave is cosine instead of sine
+      if (i % 2 == 0) {
+        yvalues[i] += p.sin(x) * 2;
+      } else {
+        yvalues[i] += p.cos(x) * 2;
       }
+      x = y;
+    }
   }
 
-  function EnergyLevels(energy, color)
-  {
-    if(energy === 0)
-    {
-    }
-    else if(energy <= 10 && energy > 0)
-    {
+  function EnergyLevels(energy, color) {
+    if (energy === 0) {
+    } else if (energy <= 10 && energy > 0) {
       p.stroke(color);
       p.noStroke();
       p.fill(color);
@@ -117,9 +99,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(708, 940);
       p.endShape(p.CLOSE);
       MakeWaveLine(343, 730, 940);
-    }
-    else if(energy <= 20 && energy > 10)
-    {
+    } else if (energy <= 20 && energy > 10) {
       p.stroke(color);
       p.noStroke();
       p.fill(color);
@@ -136,9 +116,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(760, 840);
       p.endShape(p.CLOSE);
       MakeWaveLine(210, 870, 840);
-    }
-    else if(energy <= 30 && energy > 20)
-    {
+    } else if (energy <= 30 && energy > 20) {
       p.stroke(color);
       p.noStroke();
       p.fill(color);
@@ -157,9 +135,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(850, 740);
       p.endShape(p.CLOSE);
       MakeWaveLine(140, 945, 740);
-    }
-    else if(energy <= 40 && energy > 30)
-    {
+    } else if (energy <= 40 && energy > 30) {
       p.stroke(color);
       p.noStroke();
       p.fill(color);
@@ -181,9 +157,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(870, 640);
       p.endShape(p.CLOSE);
       MakeWaveLine(105, 975, 640);
-    }
-    else if(energy <= 50 && energy > 40)
-    {
+    } else if (energy <= 50 && energy > 40) {
       p.stroke(color);
       p.fill(color);
       p.beginShape();
@@ -207,9 +181,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(900, 540);
       p.endShape(p.CLOSE);
       MakeWaveLine(95, 980, 540);
-    }
-    else if(energy <= 60 && energy > 50)
-    {
+    } else if (energy <= 60 && energy > 50) {
       p.stroke(color);
       p.line(105, 440, 975, 440);
       p.fill(color);
@@ -237,9 +209,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(880, 440);
       p.endShape(p.CLOSE);
       MakeWaveLine(105, 975, 440);
-    }
-    else if(energy <= 70 && energy > 60)
-    {
+    } else if (energy <= 70 && energy > 60) {
       p.stroke(color);
       p.fill(color);
       p.noStroke();
@@ -268,9 +238,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(850, 340);
       p.endShape(p.CLOSE);
       MakeWaveLine(140, 940, 340);
-    }
-    else if(energy <= 80 && energy > 70)
-    {
+    } else if (energy <= 80 && energy > 70) {
       p.stroke(color);
       p.fill(color);
       p.noStroke();
@@ -301,10 +269,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(800, 240);
       p.endShape(p.CLOSE);
       MakeWaveLine(210, 870, 240);
-
-    }
-    else if(energy <= 90 && energy > 80)
-    {
+    } else if (energy <= 90 && energy > 80) {
       p.stroke(color);
       p.fill(color);
       p.noStroke();
@@ -337,9 +302,7 @@ const batteryDisplay = (p) => {
       p.curveVertex(535, 140);
       p.endShape(p.CLOSE);
       MakeWaveLine(345, 745, 140);
-    }
-    else if(energy === 100)
-    {
+    } else if (energy === 100) {
       p.fill(color);
       p.noStroke();
       p.beginShape();
@@ -372,63 +335,35 @@ const batteryDisplay = (p) => {
       p.endShape(p.CLOSE);
     }
   }
-  
-  function EnergyColor(energy)
-  {
-      if(energy === 100)
-      {
-        color = p.color(0, 255, 0);
-      }
-      else if(energy > 90 && energy < 100)
-      {
-        color = p.color(50, 255, 0);
-      }
-      else if(energy <= 90 && energy > 80)
-      {
-        color = p.color(75, 255, 0);
-      }
-      else if(energy <= 80 && energy > 70)
-      {
-        color = p.color(100, 255, 0);
-      }
-      else if(energy <= 70 && energy > 60)
-      {
-        color = p.color(125, 255, 0);
-      }
-      else if(energy <= 60 && energy > 50)
-      {
-        color = p.color(150, 255, 0);
-      }
-      else if(energy <= 50 && energy > 40)
-      {
-        color = p.color(255, 200, 0);
-      }
-      else if(energy <= 40 && energy > 30)
-      {
-        color = p.color(200, 150, 0);
-      }
-      else if(energy <= 30 && energy > 20)
-      {
-        color = p.color(200, 125, 0);
-      }
-      else if(energy <= 20 && energy > 10)
-      {
-        color = p.color(255, 75, 0);
-      }
-      else if(energy <= 10 && energy > 0)
-      {
-        color = p.color(255, 50, 0);
-      }
-      else if(energy === 0)
-      {
-        color = p.color(255, 0, 0);
-      }
-      else
-      {
 
-      }
+  function EnergyColor(energy) {
+    if (energy === 100) {
+      color = p.color(0, 255, 0);
+    } else if (energy > 90 && energy < 100) {
+      color = p.color(50, 255, 0);
+    } else if (energy <= 90 && energy > 80) {
+      color = p.color(75, 255, 0);
+    } else if (energy <= 80 && energy > 70) {
+      color = p.color(100, 255, 0);
+    } else if (energy <= 70 && energy > 60) {
+      color = p.color(125, 255, 0);
+    } else if (energy <= 60 && energy > 50) {
+      color = p.color(150, 255, 0);
+    } else if (energy <= 50 && energy > 40) {
+      color = p.color(255, 200, 0);
+    } else if (energy <= 40 && energy > 30) {
+      color = p.color(200, 150, 0);
+    } else if (energy <= 30 && energy > 20) {
+      color = p.color(200, 125, 0);
+    } else if (energy <= 20 && energy > 10) {
+      color = p.color(255, 75, 0);
+    } else if (energy <= 10 && energy > 0) {
+      color = p.color(255, 50, 0);
+    } else if (energy === 0) {
+      color = p.color(255, 0, 0);
+    } else {
+    }
   }
 };
-
 
 export default batteryDisplay;
--- src/mainframe/App.js
+++ src/mainframe/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import './App.css';
-import P5Wrapper from 'react-p5-wrapper';
-import batteryDisplay from '../WelcomeScreen/batteryDisplay';
+import React from "react";
+import "./App.css";
+import P5Wrapper from "react-p5-wrapper";
+import batteryDisplay from "../WelcomeScreen/batteryDisplay";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <P5Wrapper sketch={batteryDisplay}/>
+        <P5Wrapper sketch={batteryDisplay} />
       </div>
     );
   }
 }
 
-
 export default App;
-
-
-
