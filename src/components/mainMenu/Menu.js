707c00c480df6be8366d0c42bc81346218cca366
marcocardillo2500 <marcocardillo2500@gmail.com>
2021-04-01 19:16:07 +0200

    Main menu css

--- src/components/mainMenu/Menu.js
+++ src/components/mainMenu/Menu.js
@@ -1,7 +1,10 @@
 import '../../App.css';
+import styled from 'styled-components';
 import React, { Component } from 'react';
 import { ReactSVG } from 'react-svg';
-import Background, {CablesArea, Potentiometer} from './items/components';
+import EnergySVG from '../../svg/Energy.svg';
+
+import Background, {CablesArea,ForCircle, Potentiometer, Button, Energy} from './items/components';
 
 class Menu extends Component {
   // handle svg file with arrows
@@ -11,12 +14,119 @@ class Menu extends Component {
 
   render(){
     return (
-      <div className="equi">
-        <Background>
-          <Potentiometer/>
-          <CablesArea/>
-        </Background>
-      </div>
+      <div className="equi" >
+        <div className="equiBackground">
+        <div className="potentiometerPosition"/>
+        <div className="rectangleCable"/>
+              <div className="grid2x2">
+                    <div>
+                    <ReactSVG
+                    afterInjection={(error, svg) => {
+                      if (error) {
+                        console.error(error)
+                        return
+                      }
+                      console.log(svg)
+                    }}
+                    beforeInjection={(svg) => {
+                      svg.classList.add('svg-class-name')
+                      svg.setAttribute('style', 'width: 300')
+                     
+                    }}
+                    evalScripts="always"
+                    fallback={() => <span>Error!</span>}
+                    loading={() => <span>Loading</span>}
+                    onClick={() => {
+                      console.log('wrapper onClick')
+                    }}
+                    renumerateIRIElements={false}
+                    src={EnergySVG}
+                    useRequestCache={false}
+                    wrapper="div"/>
+                   </div>
+                   <div>
+                    <ReactSVG
+                    afterInjection={(error, svg) => {
+                      if (error) {
+                        console.error(error)
+                        return
+                      }
+                      console.log(svg)
+                    }}
+                    beforeInjection={(svg) => {
+                      svg.classList.add('svg-class-name')
+                      svg.setAttribute('style', 'width: 300')
+                   
+                    }}
+                    evalScripts="always"
+                    fallback={() => <span>Error!</span>}
+                    loading={() => <span>Loading</span>}
+                    onClick={() => {
+                      console.log('wrapper onClick')
+                    }}
+                    renumerateIRIElements={false}
+                    src={EnergySVG}
+                    useRequestCache={false}
+                    wrapper="div"/>
+                    </div>
+                    <div>
+                    <ReactSVG
+                    afterInjection={(error, svg) => {
+                      if (error) {
+                        console.error(error)
+                        return
+                      }
+                      console.log(svg)
+                    }}
+                    beforeInjection={(svg) => {
+                      svg.classList.add('svg-class-name')
+                      svg.setAttribute('style', 'width: 300')
+                      
+                    }}
+                    evalScripts="always"
+                    fallback={() => <span>Error!</span>}
+                    loading={() => <span>Loading</span>}
+                    onClick={() => {
+                      console.log('wrapper onClick')
+                    }}
+                    renumerateIRIElements={false}
+                    src={EnergySVG}
+                    useRequestCache={false}
+                    wrapper="div"/>
+                   </div>
+                   <div>
+                    <ReactSVG
+                    afterInjection={(error, svg) => {
+                      if (error) {
+                        console.error(error)
+                        return
+                      }
+                      console.log(svg)
+                    }}
+                    beforeInjection={(svg) => {
+                      svg.classList.add('svg-class-name')
+                      svg.setAttribute('style', 'width: 300')
+                      
+                    }}
+                    evalScripts="always"
+                    fallback={() => <span>Error!</span>}
+                    loading={() => <span>Loading</span>}
+                    onClick={() => {
+                      console.log('wrapper onClick')
+                    }}
+                    renumerateIRIElements={false}
+                    src={EnergySVG}
+                    useRequestCache={false}
+                    wrapper="div"/>
+                    </div>
+              
+               
+              </div>
+              </div>
+              </div>
+           
+      
+     
     );
   }
 }
---
+++ src/components/mainMenu/items/components.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+
+const Equi = styled.div`
+background: black;
+border-radius: 50%;
+width: 1080px;
+height: 1080px;
+border: 5px solid green;
+margin-top: 120px;
+position: absolute;
+display: flex;
+flex-direction:row;
+justify-content: center;
+`
+
+export const CablesArea = styled.div`
+height: 370px;
+width: 100px;
+background: white;
+`
+
+export const Potentiometer = styled.div`
+background: white;
+border-radius: 50%;
+width: 400px;
+height: 400px;
+margin-top: 340px;
+position: absolute;
+`
+export const Button = styled.div`
+background: white;
+border-radius: 50%;
+width: 150px;
+height: 150px;
+border: 5px solid red;
+margin-top: 60%;
+position: absolute;
+display: flex;
+justify-content: center;
+`
+
+
+export const ForCircle = styled.div`
+display:flex;
+flex-direction:column;
+border:10px solid yellow;
+margin-left:-50%;
+`
+export default Equi;
