d1a13b9ab53d33d19fe71ba49aff5cca8c13e693
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-04-17 13:38:44 +0200

    Boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { authReducer, AUTH_FEATURE_KEY } from "./store/auth.slice";
+
+const store = configureStore({
+  reducer: {
+    [AUTH_FEATURE_KEY]: authReducer,
+  },
+  middleware: [...getDefaultMiddleware()],
+  devTools: process.env.NODE_ENV !== "production",
+  enhancers: [],
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
