0f56eb1b702d472f696c96f7f19a9c2dd1a90200
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-10-12 22:12:30 +0200

    Update enphase dummy data end point

--- apps/librium/src/apps/enphase/controllers.py
+++ apps/librium/src/apps/enphase/controllers.py
@@ -33,5 +33,5 @@ def batteryState() -> Literal["idle", "charging", "discharging"]:
 @enphase_blueprint.route("/data", methods=['POST'])
 def data():
     data = request.json
-    print(json.dumps(jsonify(data)))
+    print(json.dumps(data))
     return jsonify(data)
