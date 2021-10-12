89af564aa64b7490ec3e8172a59d9616084a0802
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2021-10-12 20:08:35 +0200

    Fix current time on the dial rotator of Equi

--- libs/equi/scheduler/utils/src/lib/angle-to-time.ts
+++ libs/equi/scheduler/utils/src/lib/angle-to-time.ts
@@ -1,9 +1,10 @@
 export const angleToTime = (angle: number) => {
+  const currentTime = new Date();
   const t = (angle * 24 * 60) / 360;
-  const hour = Math.floor(t / 60)
+  const hour = (Math.floor((t + currentTime.getHours()) / 60) % 24)
     .toString()
     .padStart(2, '0');
-  const min = Math.floor(t % 60)
+  const min = Math.floor((t + currentTime.getMinutes()) % 60)
     .toString()
     .padStart(2, '0');
   return hour + ':' + min;
