6fdc45557e603657c625761ed668d21a2c447365
Nhan Nguyen <32163916+NguyenVuNhan@users.noreply.github.com>
2021-12-06 17:06:54 +0100

    Update teensy

--- apps/teensy/src/main.py
+++ apps/teensy/src/main.py
@@ -17,6 +17,29 @@ async def teensyReader():
         await sendMessage(line)
 
 
+class OutputProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        print('port opened', transport)
+
+    def data_received(self, data):
+        print('data received', repr(data))
+        line = str(line, 'utf-8').strip()
+        sendMessage(line)
+
+    def connection_lost(self, exc):
+        print('port closed')
+        self.transport.loop.stop()
+
+    def pause_writing(self):
+        print('pause writing')
+        print(self.transport.get_write_buffer_size())
+
+    def resume_writing(self):
+        print(self.transport.get_write_buffer_size())
+        print('resume writing')
+
+
 async def sendMessage(message):
     logging.debug(message)
     if clients:
@@ -55,7 +78,8 @@ if __name__ == "__main__":
     # register the async tasks
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_server)
-    loop.run_until_complete(teensyReader())
+    coro = serial_asyncio.create_serial_connection(loop, OutputProtocol, '/dev/ttyUSB0', baudrate=9600)
+    transport, protocol = loop.run_until_complete(coro)
 
     # run all the async
     loop.run_forever()
